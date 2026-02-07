4f25762303ee77a84caa8f09efd3586602cf1e60
Sterling Kooshesh <sterling@retrolancers.com>
2026-02-06 23:41:06 -0600

    chore: removed login stuff. App is meant to be run locally.

--- AIInterviewer.Client/src/components/NavHeader.vue
+++ AIInterviewer.Client/src/components/NavHeader.vue
@@ -24,39 +24,7 @@
               <RouterLink to="/interviews/history" class="p-4 flex items-center justify-start mw-full hover:text-sky-500 dark:hover:text-sky-400" ActiveClass="text-blue-700 dark:text-blue-300">Interview History</RouterLink>
             </li>
            
-            <template v-if="hasRole('Admin')">
-              <li class="relative flex flex-wrap just-fu-start m-0">
-                <RouterLink to="/admin/" class="p-4 flex items-center justify-start mw-full hover:text-sky-500 dark:hover:text-sky-400" ActiveClass="text-blue-700 dark:text-blue-300">Admin</RouterLink>
-              </li>
-            </template>
-            <template v-if="user">
-              <li>
-                <div class="mx-3 relative">
-                  <div>
-                    <RouterLink to="/profile"
-                       class="max-w-xs bg-white dark:bg-black rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 lg:p-2 lg:rounded-md lg:hover:bg-gray-50 dark:lg:hover:bg-gray-900 dark:ring-offset-black" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                      <img class="h-8 w-8 rounded-full" :src="user.profileUrl" alt="">
-                      <span class="hidden ml-3 text-gray-700 dark:text-gray-300 text-sm font-medium lg:block">
-                            <span class="sr-only">Open user menu for </span>
-                            {{user.userName}}
-                        </span>
-                    </RouterLink>
-                  </div>
-                </div>
-              </li>
-              <li class="mr-3 relative flex flex-wrap just-fu-start m-0">
-                <SecondaryButton @click="signout($router)">
-                  Sign Out
-                </SecondaryButton>
-              </li>
-            </template>
-            <template v-else>
-              <li class="relative flex flex-wrap just-fu-start m-0">
-                <RouterLink to="/Account/Login" class="m-2 mr-4">
-                  <SecondaryButton class="m-2" href="/signin">Sign In</SecondaryButton>
-                </RouterLink>
-              </li>
-            </template>
+            
 
             <li class="relative flex flex-wrap just-fu-start m-0">
               <DarkModeToggle />
